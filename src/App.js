7de46641bd3cc2629a8ffdd3debd67f580eeae05
tbriggs1 <tombriggs93@protonmail.com>
2021-02-22 21:29:03 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import TestComponent from './TestComponent';
 
 function App() {
+  
+  //Write basic JS code
+  const items = [1, 2, 3, 4, 5];
+
+  items.map((item) => {
+    console.log(item);
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+     <TestComponent />
+   </div>
   );
 }
 
