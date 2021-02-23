e0967cb4614ced5d1aab421c6134b71d699fe169
tbriggs1 <tombriggs93@protonmail.com>
2021-02-23 21:11:27 +0000

    Uplifting state

--- src/components/CreateTweet.js
+++ src/components/CreateTweet.js
@@ -3,15 +3,21 @@ import React, {useState} from 'react';
 const CreateTweet = (props) => {
     //state
     const [textInput, setTextInput] = useState('');
-
+    const [tweets, setTweets] = useState([]);
 
     const userInputHandler = (e) => {
         //e.target.value gets the current text that you are typing
         setTextInput(e.target.value);
     }
 
+    const submitTweetHandler = (e) => {
+        e.preventDefault();
+        setTweets([...tweets, textInput]);
+        setTextInput("");
+    }
+
     return(
-        <form>
+        <form onSubmit={submitTweetHandler}>
             <textarea value={textInput} onChange={userInputHandler} cols="40" rows="10"></textarea>
             <button>Submit</button>
         </form>
